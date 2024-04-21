0d904989af4c7b49faca2732cb8d684de1fbddf8
Lakshya Agrawal <lakshya424agrawal@gmail.com>
2024-04-21 23:18:51 +0530

    update the ports

--- src/app.js
+++ src/app.js
@@ -72,7 +72,7 @@ try{
     console.log(err);
 }
 
-const port = process.env.PORT || 3000;
+const port = process.env.APP_PORT || 3000;
 // Start server
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
